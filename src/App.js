87bdf76af8419d358a3521fd3a5ebe2d56091151
Master52 <harajmast9008@gmail.com>
2021-01-27 07:21:42 +0530

    Fixed Routing  issue

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={window.location.pathname || ""}>
       <div className="App">
         <Header />
         <Switch>
